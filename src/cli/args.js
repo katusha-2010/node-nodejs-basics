3bdadc4d594a615c6663035f4fe46f930781183a
katusha-2010 <katusha-2010@mail.ru>
2022-11-29 16:31:43 +0300

    feat: add decisions for args.js and env.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const commandLineArgs = process.argv;
+  let result = "";
+  let i = 0;
+  for (let item of commandLineArgs) {
+    if (item.includes("--")) {
+      result += `${item.slice(2)} is ${commandLineArgs[i + 1]}, `;
+    }
+    i++;
+  }
+  console.log(result.trim().slice(0, -1));
+  // Write your code here
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+  const variables = process.env;
+  for (let key in variables) {
+    if (key.includes("RSS_")) console.log(`${key} = ${variables[key]}`);
+  }
+  // Write your code here
 };
 
 parseEnv();
