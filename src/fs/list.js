9805e68581f3732550aa54f54e6a7acefa90c028
katusha-2010 <katusha-2010@mail.ru>
2022-11-29 14:51:10 +0300

    feat: add decisions for list.js, read.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,35 @@
+import * as path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { access, readdir } from "fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+async function isExist(pathToItem) {
+  try {
+    await access(pathToItem);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
 const list = async () => {
-    // Write your code here 
+  const pathToFolder = path.join(__dirname, "files");
+  try {
+    if (!(await isExist(pathToFolder))) {
+      throw Error("FS operation failed");
+    } else {
+      const folderContent = await readdir(pathToFolder);
+      for (let item of folderContent) {
+        process.stdout.write(`${item}\n`);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  // Write your code here
 };
 
 await list();
