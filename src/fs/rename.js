1d15b04ca38e9f8a0c0b265a1851f6648a1c5028
katusha-2010 <katusha-2010@mail.ru>
2022-11-29 14:13:46 +0300

    feat: add decisions for delete.js and rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,37 @@
-const rename = async () => {
-    // Write your code here 
+import * as path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { access, rename } from "fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+async function isExist(pathToItem) {
+  try {
+    await access(pathToItem);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+const renameFunc = async () => {
+  const pathToFile = path.join(__dirname, "files", "wrongFilename.txt");
+  const pathToRenamedFile = path.join(__dirname, "files", "properFilename.md");
+
+  try {
+    if (
+      (await isExist(pathToRenamedFile)) === true ||
+      (await isExist(pathToFile)) === false
+    ) {
+      throw Error("FS operation failed");
+    } else {
+      await rename(pathToFile, pathToRenamedFile);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+  // Write your code here
 };
 
-await rename();
+await renameFunc();
