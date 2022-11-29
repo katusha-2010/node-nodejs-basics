73c4be3076c3c185ad353ade1a7fa4ea0913efda
katusha-2010 <katusha-2010@mail.ru>
2022-11-29 13:53:13 +0300

    feat: add solution for copy.js and create.js tasks

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,44 @@
+import * as path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { access, readdir, cp, copyFile } from "fs/promises";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+async function isExist(pathToItem) {
+  try {
+    await access(pathToItem);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
 const copy = async () => {
-    // Write your code here 
+  const pathToFolder = path.join(__dirname, "files");
+  const pathToFolderCopy = path.join(__dirname, "files_copy");
+  try {
+    if (
+      (await isExist(pathToFolder)) === false ||
+      (await isExist(pathToFolderCopy)) === true
+    ) {
+      throw Error("FS operation failed");
+    } else {
+      const folderContent = await readdir(pathToFolder);
+
+      for (let item of folderContent) {
+        await cp(
+          path.join(__dirname, "files", `${item}`),
+          path.join(__dirname, "files_copy", `${item}`),
+          { recursive: true }
+        );
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+  // Write your code here
 };
 
 copy();
